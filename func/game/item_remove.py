7cd61c250a96b94454526fa4bc9f10058657c31c
oleg-medovikov <oleg.medovikov@mail.ru>
2023-11-21 12:26:14 +0300

    remove item func

--- func/game/item_remove.py
+++ func/game/item_remove.py
@@ -1,26 +1,66 @@
+from sqlalchemy import and_
+
 from mdls import Person, Inventory, Item
 from conf import MAX_BAG_CAPASITY
-from func import person_change
+from func.game.person_change import person_change
 
 
-async def item_remove(person: "Person", inventory: "Inventory", item: "Item"):
+async def item_remove(
+    person: "Person", inventory: "Inventory", item: "Item"
+) -> tuple[bool, str, dict]:
     """
     нужно проверить 2 вещи:
     есть ли пустое место в сумке, куда поместить вещь
     если у вещи есть альтернатива, ее можно снять только
     в случае наличия этой вещи в сумке
+    Необходимо проверить наличие, этой вещи, что она подходит по требованиям,
+    предложить выбор, если их несколько
+    и сразу после remove запустить using
+    чтобы случилось переодевание
     """
 
     if len(inventory.bag) >= MAX_BAG_CAPASITY:
-        return False, "Сумка заполнена, некуда поместить данный предмет"
+        return False, "Сумка заполнена, некуда поместить данный предмет", {}
 
     if item.alternative is not None:
-        if item.alternative not in inventory.bag:
-            return (
-                False,
-                f"Вы не можете снять {item.name}, так как у Вас нет альтернативы.",
-            )
+        # если alternative -1 - подойтет любой предмет в этот слот
+        # если конкретное число - только предмет с таким id
+        if item.alternative > 0 and item.alternative not in inventory.bag:
+            return False, item.mess_remove_fail, {}
+        # необходимо вытащить предметы из базы, чтобы узнать их слоты
+        alter_items = await Item.query.where(
+            and_(Item.id.in_(inventory.bag), Item.slot == item.slot)
+        ).gino.all()
+        dict_ = {}
+        for _ in alter_items:
+            dict_[_.id] = _.name
+        if len(dict_):
+            # тут мы должны снять предмет, а для начала снять эффект
+            effect = {}
 
+            for key, value in item.effect:
+                effect[key] = -value
+
+            person = await person_change(person, effect)
+            # теперь удаляем из слота и кидаем в сумку
+            inventory.bag.append(item.id)
+            if item.slot in ["head", "neck", "earrings", "body", "legs", "shoes"]:
+                inv_ = {item.slot: None, "bag": inventory.bag}
+            if item.slot == "onehand":
+                inventory.hands.remove(item.id)
+                inv_ = {"hands": inventory.hands, "bag": inventory.bag}
+            if item.slot == "twohands":
+                inv_ = {"hands": [], "bag": inventory.bag}
+            if item.slot == "rings":
+                inventory.rings.remove(item.id)
+                inv_ = {"rings": inventory.rings, "bag": inventory.bag}
+
+            await inventory.update(**inv_).apply()
+            return True, item.mess_remove, dict_
+        else:
+            return False, item.mess_remove_fail, {}
+
+    # тут разбираем предметы, для которых не нужна альтернатива
     # теперь нужно убрать эффекты предмета
     effect = {}
 
@@ -28,3 +68,20 @@ async def item_remove(person: "Person", inventory: "Inventory", item: "Item"):
         effect[key] = -value
 
     person = await person_change(person, effect)
+
+    # теперь удаляем из слота и кидаем в сумку
+    inventory.bag.append(item.id)
+    if item.slot in ["head", "neck", "earrings", "body", "legs", "shoes"]:
+        inv_ = {item.slot: None, "bag": inventory.bag}
+    if item.slot == "onehand":
+        inventory.hands.remove(item.id)
+        inv_ = {"hands": inventory.hands, "bag": inventory.bag}
+    if item.slot == "twohands":
+        inv_ = {"hands": [], "bag": inventory.bag}
+    if item.slot == "rings":
+        inventory.rings.remove(item.id)
+        inv_ = {"rings": inventory.rings, "bag": inventory.bag}
+
+    await inventory.update(**inv_).apply()
+
+    return True, item.mess_remove, {}
