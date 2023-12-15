59683784c107349175664058e6aec17b802371cf
oleg-medovikov <oleg.medovikov@mail.ru>
2023-12-15 12:19:20 +0300

    отдельная логика для изменения максимума рассудка и здоровья

--- func/game/person_change.py
+++ func/game/person_change.py
@@ -14,8 +14,6 @@ L_STANDART = [
     "money",
     "health",
     "mind",
-    "health_max",
-    "mind_min",
 ]
 L_POSITIVE = ["hunger", "weary"]
 L_ID = ["location"]
@@ -35,6 +33,19 @@ async def person_change(person: "Person", DICT: dict, negative=False) -> "Person
 
         if negative and isinstance(value, int):
             value = -value
+
+        # отдельная логика для изменения максимума здоровья и рассудка
+        # рассудок и здоровье меняется в относительных значениях от предыдущего значения
+        # формула: x2 = x1*(old + value)/old с обычным округлением до целого
+        if key in ["health_max", "mind_max"]:
+            key_x = key.replace("_max", "")
+            x1 = getattr(person, key_x)
+            up = {
+                key: old + value,
+                key_x: int(round(x1 * (old + value) / old)),
+            }
+            person = await person.update(**up).apply()
+            continue
 
         itog = {
             # стандартно складываем значения
