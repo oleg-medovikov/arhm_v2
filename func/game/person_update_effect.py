2980add7fc103793e8e2c87a86ec6c93c7d966fa
oleg-medovikov <oleg.medovikov@mail.ru>
2023-12-26 17:19:39 +0300

    person_update_effect

---
+++ func/game/person_update_effect.py
@@ -0,0 +1,7 @@
+from mdls import Person, PersonEffect
+
+
+async def person_update_effect(person: Person) -> "Person":
+    """
+    работаем со временными эффектами,
+    """
--- func/system/update_sticker.py
+++ func/system/update_sticker.py
@@ -20,12 +20,16 @@ async def update_sticker(chat_id: int, sticker_name: Optional[str], bot: Bot):
     if sticker_name is not None:
         sticker = await Sticker.query.where(Sticker.name == sticker_name).gino.first()
         if sticker is not None:
-            MESS = await bot.send_sticker(
-                chat_id, sticker=sticker.send_id, protect_content=True
-            )
-            if log is not None:
-                await log.update(message_id=MESS.message_id).apply()
-            else:
-                await StickerLog.create(
-                    chat_id=chat_id, message_id=MESS.message_id, name=sticker_name
+            try:
+                MESS = await bot.send_sticker(
+                    chat_id, sticker=sticker.send_id, protect_content=True
                 )
+            except TelegramBadRequest:
+                pass
+            else:
+                if log is not None:
+                    await log.update(message_id=MESS.message_id).apply()
+                else:
+                    await StickerLog.create(
+                        chat_id=chat_id, message_id=MESS.message_id, name=sticker_name
+                    )
